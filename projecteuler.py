98e5268be24f246c44172e0d38354d27e22cc852
Liu JiaJun <liujj90@gmail.com>
2016-09-16 00:06:50 +0930

    solved problem 17

--- projecteuler.py
+++ projecteuler.py
@@ -685,3 +685,67 @@ def sumPowerDigit(power):
 	for i in x:
 		summed += int(i)
 	return summed
+
+'''Number letter counts
+Problem 17
+If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 
+(one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+'''
+
+''' solution adapted from:
+ http://codereview.stackexchange.com/questions/107851/project-euler-no-17-counting-letters-to-write-the-numbers-from-1-to-1000'''
+
+
+NumbersDict = {
+	n: len(word) for (n, word) in {
+	0: '',
+	1: 'one',
+	2: 'two',
+	3: 'three',
+	4: 'four',
+	5: 'five',
+	6: 'six',
+	7: 'seven',
+	8: 'eight',
+	9: 'nine',
+	10: 'ten',
+	11: 'eleven',
+	12: 'twelve',
+	13: 'thirteen',
+	14: 'fourteen',
+	15: 'fifteen',
+	16: 'sixteen',
+	17: 'seventeen',
+	18: 'eighteen',
+	19:'nineteen',
+	20: 'twenty',
+	30: 'thirty',
+	40: 'forty',
+	50: 'fifty',
+	60: 'sixty',
+	70: 'seventy',
+	80: 'eighty',
+	90: 'ninety'
+	}.iteritems()
+}
+
+def number_length(number):
+	
+	if number < 21:
+		return NumbersDict[number]
+	elif number < 100:
+		tens, ones = divmod(number, 10)
+		return NumbersDict[10* tens] + NumbersDict[ones]
+	elif number < 1000:
+		hundreds , rest = divmod(number, 100)
+		#tens, ones = divmod(rest, 10)
+		return NumbersDict[hundreds] + len('hundred')+ (len('and') + number_length(rest) if rest else 0)
+	else:
+		thousands, rest = divmod(number, 1000)
+		return number_length(thousands) + len('thousand') + number_length(rest)
+print(sum(number_length(number) for number in range (1, 1001)))
