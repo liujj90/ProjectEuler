11584ba53efcf087a5519e6b9a445966a4371a53
Liu JiaJun <liujj90@gmail.com>
2016-10-01 23:58:07 +0930

    Solved Problem 25

--- projecteuler.py
+++ projecteuler.py
@@ -988,9 +988,46 @@ permute = sorted(permute)
 permute = list(permute)
 permute[int(1e6)-1]
 
+'''
+1000-digit Fibonacci number
+Problem 25
+The Fibonacci sequence is defined by the recurrence relation:
 
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
 
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
 
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?'''
 
+def fib(n):
+	a = 1
+	b = 0
+	while n >1:
+		a, b = a+b, a
+		n = n-1 # count down from supplied n till n ==0
+	print a
 
+# check length instead
 
+a = 1
+b = 0
+n = 1
+
+while len(str(a)) != 1000:
+	a, b = a+b, a
+	n = n + 1
+else :
+	print a,n
